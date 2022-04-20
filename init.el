ce2f24d7fb14ed03c7c42225d4605dc03725d337
Andrej Lamov <andrej.lamov@gmail.com>
2022-04-20 08:56:13 +0200

    Some good default from minad

--- init.el
+++ init.el
@@ -107,6 +107,22 @@
 ;; Truncate lines toggle.
 (bind-key* "M-SPC a t" 'toggle-truncate-lines)
 
+(defun crm-indicator (args)
+  (cons (concat "[CRM] " (car args)) (cdr args)))
+(advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+;; Do not allow the cursor in the minibuffer prompt
+(setq minibuffer-prompt-properties
+      '(read-only t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+;; Vertico commands are hidden in normal buffers.
+;; (setq read-extended-command-predicate
+;;       #'command-completion-default-include-p)
+
+;; Enable recursive minibuffers
+(setq enable-recursive-minibuffers t)
 
 ;; The light modus theme.
 (use-package modus-themes
